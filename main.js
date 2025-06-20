e1181792812c35fa6edece1b14f1194f912d7592
matxbox <matxbox@rocketmail.com>
2025-06-20 18:22:22 +0200

    implemented addUser route

--- main.js
+++ main.js
@@ -24,17 +24,20 @@ app.use((req, res, next) => {
         return next()
     }
     try {
-        const decoded_jwt = jwt.verify(req.headers.authorization.split(" ")[1], env.jwt.secret_key)
-        if (decoded_jwt.user.toLowerCase() === "mattia") {
-            return next()
+        if (req.headers.authorization != null) {
+            const decoded_jwt = jwt.verify(req.headers.authorization.split(" ")[1], env.jwt.secret_key)
+            if (decoded_jwt.user.toLowerCase() === "mattia") {
+                return next()
+            } else {
+                res.status(403).json({ success: false, token: decoded_jwt })
+            }
         } else {
-            res.status(403).json({ success: false, token: decoded_jwt })
+            res.status(401).send("Missing authentication")
         }
-
     } catch (error) {
 
         console.error(error)
-        res.send(error.toString())
+        res.status(400).send(error.toString())
     }
 })
 
@@ -71,30 +74,53 @@ app.post("/addFilm", async (req, res) => {
 
 app.post("/login", async (req, res) => {
     const { username, password } = req.body
-    if (!(username === undefined || password === undefined)) {
+    //password = decodeURIComponent(password)
+    if (username !== undefined && password !== undefined) {
         try {
             await client.connect()
             const user_match = await client.db("sample_mflix").collection("users").findOne({ name: username })
-            if (user_match !== null) {
-                const test = await bcrypt.compare(password, "$2b$12$j0jvmji7RgMNbcOkL6H4SuH3EEbJPyNollMurYnU22/Vu.BdxMkxG") // provolona
+            if (user_match != null) {
+                const test = await bcrypt.compare(password, user_match.password) // provolona
                 if (test) {
-                    token = jwt.sign({ user: username }, env.jwt.secret_key, { expiresIn: "1h" })
+                    const token = jwt.sign({ user: username }, env.jwt.secret_key, { expiresIn: "1h" })
                     res.status(200).send({ token: token, match: user_match })
+                } else {
+                    res.status(403).send("Wrong username or password")
                 }
             } else {
                 res.status(403).send("Wrong username or password")
             }
-
         } catch (error) {
             console.error(error)
             res.status(500).send(error.toString())
         } finally {
             await client.close()
         }
-
-
     } else {
         res.status(400).send("Request does not contain mandatory login information")
+    }
+})
+
+app.put("/addUser", async (req, res) => {
+    const { username, password, email } = req.body
+    if (username !== undefined && password !== undefined && email !== undefined) {
+        try {
+            await client.connect()
+            const user_match = await client.db("sample_mflix").collection("users").findOne({ $or: [{ name: username }, { email: email }] })
+            if (user_match == null) {
+                const pass_crypt = await bcrypt.hash(password, 12)
+                const result = await client.db("sample_mflix").collection("users").insertOne({ name: username, email: email, password: pass_crypt })
+                res.status(201).json({ success: true, film_data: req.body, new_id: result.insertedId })
+            } else {
+                res.status(401).send("user/email already registered")
+            }
+        } catch (error) {
+            res.status(500).send(error.toString())
+        } finally {
+            await client.close()
+        }
+    } else {
+        res.status(400).send("Request does not contain mandatory information: username, password, email")
     }
 })
 
